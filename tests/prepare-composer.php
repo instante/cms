37eaed93bfd7206dfd7b9e2e269d46689a01ecdb
Richard Ejem <richard@ejem.cz>
2015-10-12 23:20:48 +0200

    fixed composer.json generation

--- tests/prepare-composer.php
+++ tests/prepare-composer.php
@@ -4,13 +4,14 @@ $rootDir = __DIR__ . '/..';
 $testsDir = __DIR__;
 
 if (getenv('NETTE') !== 'default') {
-    $composerJson = json_decode(file_get_contents($composerFile));
+    $composerFile = $rootDir . '/composer.json';
+    $composerJson = json_decode(file_get_contents($composerFile), TRUE);
     foreach ($composerJson['require'] as $key => &$val) {
-        if (preg_match('^(?:nette|latte|tracy)/', $key)) {
+        if (preg_match('~^(?:nette|latte|tracy)/~', $key)) {
             $val = getenv('NETTE');
         }
     }
-    file_put_contents($rootDir . '/composer.json', json_encode($composerJson));
+    file_put_contents($composerFile, json_encode($composerJson));
 
     echo 'Using nette components version '.getenv('NETTE');
 
